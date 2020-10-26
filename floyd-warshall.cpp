7ac18b5231935c3af984630c59b0a973376213db
elPeiretti <73491345+elPeiretti@users.noreply.github.com>
2020-10-26 14:20:41 -0300

    Update floyd-warshall.cpp

--- floyd-warshall.cpp
+++ floyd-warshall.cpp
@@ -1,23 +1,3 @@
-#include <iostream>
-#include <bits/stdc++.h>
-#include <algorithm>
-using namespace std;
-
-#define forn(i,n) for(int i=0;i<int(n);i++)
-#define forsn(i,s,n) for(int i=int(s);i<int(n);i++)
-#define dforn(i,n) for(int i=int(n)-1;i>=0;i--)
-#define dforsn(i,s,n) for(int i=int(n)-1;i>=int(s);i--)
-#define forall(i,c) for(auto i = (c).begin(); i != (c).end(); i++)
-#define dforall(i,c) for(auto i = (c).rbegin(); i != (c).rend(); i++)
-#define all(c) (c).begin(),(c).end()
-#define DBG(x) cout << #x << " = " << (x) << endl
-#define RAYA arch << "===============================" << endl
-typedef long long ll;
-typedef long double ld;
-typedef vector<int> vi;
-typedef vector<vector<int>> vii;
-typedef pair<int,int> par;
-
 vector<vector<int>>dist;
 //se fija todos los caminos posibles entre i y j, viendo si
 //el camino con el nodo intermedio K es mas corto o no.
@@ -33,18 +13,6 @@ void floydW(int n){
 		}
 	}
 }
-
-int main(int argc, char *argv[]) {
-	//ofstream arch;
-	//arch.open("mila.txt");
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	
-	return 0;
-}
-
 
 
 
