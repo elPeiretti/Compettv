964abc9f29c20e4bf6133662cb64c1cd7c0fc8b7
Tomás Peiretti <73491345+elPeiretti@users.noreply.github.com>
2020-10-26 15:47:44 -0300

    Create DFS.cpp

---
+++ grafos/Recorridos/DFS.cpp
@@ -0,0 +1,12 @@
+vector<int>listaAdy[1000000];
+//o set tambien se puede usar
+bitset<1000000>used;
+
+void dfs(int node){
+	used.set(node);
+	for(auto it = listaAdy[node].begin(); it!=listaAdy[node].end(); ++it){
+		if(!used[*it]){
+			dfs(*it);
+		}
+	}
+}
