1e2c66b082907f5d6c0218c292f82af651f2e5a9
Tomás Peiretti <73491345+elPeiretti@users.noreply.github.com>
2020-12-07 20:55:49 -0300

    Create 2d-sum.cpp

---
+++ Range Queries/Fenwick Tree/2d-sum.cpp
@@ -0,0 +1,51 @@
+#define MAXN 10000
+int bit[MAXN][MAXN];
+
+void update(int x, int y, int val){
+  for(;x<MAXN; x+= x&-x){
+    for(int j=y ; j<MAXN; j+= j&-j){
+	      bit[x][j] += val;
+    }
+  }
+}
+
+int getaux(int x, int y){
+  int ans=0;
+
+  for(; x; x-=x&-x){
+	  for(int j=y; j; j-=j&-j){
+		  ans+=bit[x][j];
+	  }
+  }
+  return ans;
+}
+
+int get(int x1, int y1, int x2, int y2){
+	return getaux(x2,y2)- getaux(x1-1,y2) - getaux(x2,y1-1) + getaux(x1-1,y1-1);
+}
+	
+	
+	
+int main(int argc, char *argv[]) {
+
+	int filas, columnas;
+  
+	forsn(i,1,filas+1){ //inicializa la matriz
+		forsn(j,1,columnas+1){
+			int x; cin>>x;
+			update(i,j,x);
+		}
+	}
+		
+		cout<<get(1,1,5,5);
+		
+		return 0;
+}
+			
+			
+			
+			
+			
+			
+			
+		 	
