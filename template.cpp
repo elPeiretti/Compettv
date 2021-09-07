7ee640a950bd15a308f91ba4ffcca64a5ef2c805
Tomás Peiretti <73491345+elPeiretti@users.noreply.github.com>
2021-09-07 18:54:42 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,21 +5,16 @@ using namespace std;
 
 #define forn(i,n) for(int i=0;i<int(n);i++)
 #define forsn(i,s,n) for(int i=int(s);i<int(n);i++)
-#define dforn(i,n) for(int i=int(n)-1;i>=0;i--)
-#define dforsn(i,s,n) for(int i=int(n)-1;i>=int(s);i--)
-#define forall(i,c) for(auto i = (c).begin(); i != (c).end(); i++)
-#define dforall(i,c) for(auto i = (c).rbegin(); i != (c).rend(); i++)
+#define dbg(x) cerr << #x << " = " << (x) << endl
 typedef long long ll;
 typedef long double ld;
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 typedef pair<int,int> ii;
 
-
-
 int main(int argc, char *argv[]) {
-	//ofstream arch;
-	//arch.open("mila.txt");
+	//ifstream cin;
+	//cin.open("mila.txt");
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
